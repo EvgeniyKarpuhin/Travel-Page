8acf34d85e4f6dd0b4b57c29490375f5761ef1d2
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2022-06-23 15:31:16 +0300

    add

--- js/style.js
+++ js/style.js
@@ -22,3 +22,18 @@
         menu.classList.remove('header__nav-active');
     })
 }())
+
+//Scroll to anchors
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
