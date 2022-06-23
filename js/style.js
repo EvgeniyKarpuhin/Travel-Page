5c1857e114bb019f84ea8cbff1fc6ece95567997
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2022-06-23 16:03:23 +0300

    add

--- js/style.js
+++ js/style.js
@@ -1,10 +1,13 @@
 (function () {
     const header = document.querySelector('.header');
+    const headerWrapper = document.querySelector('.header__wrapper');
     window.onscroll = () => {
-        if (window.pageYOffset > 200) {
+        if (window.pageYOffset > 100) {
             header.classList.add('header_active');
+            headerWrapper.classList.add('header__wrapper-active');
         } else {
             header.classList.remove('header_active');
+            headerWrapper.classList.remove('header__wrapper-active');
         }
     }
 }());
